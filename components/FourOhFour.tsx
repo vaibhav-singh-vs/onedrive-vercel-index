b5883b9be0708c984ae9b25928bb9182a1226265
vaibhav-singh-vs <73382757+vaibhav-singh-vs@users.noreply.github.com>
2022-08-02 17:15:37 +0530

    Update FourOhFour.tsx

--- components/FourOhFour.tsx
+++ components/FourOhFour.tsx
@@ -23,15 +23,7 @@ const FourOhFour: React.FC<{ errorMsg: string }> = ({ errorMsg }) => {
             <kbd className="rounded border border-gray-400/20 bg-gray-100 px-1 font-mono text-xs dark:bg-gray-800">
               F12
             </kbd>{' '}
-            and open devtools for more details, or seek help at{' '}
-            <a
-              className="text-blue-600 hover:text-blue-700 hover:underline"
-              href="https://github.com/spencerwooo/onedrive-vercel-index/discussions"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              onedrive-vercel-index discussions
-            </a>
+            and open devtools for more details.
             .
           </Trans>
         </div>
